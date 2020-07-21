ca8aa257e5745ac3df1f1f1f5b9263e209e4cf5e
shrestha1shreejan <sthasgn1@gmail.com>
2020-07-21 17:49:38 -0500

    added global error handling

---
+++ DatingSiteBackend/Extensions/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,32 @@
+﻿using FileLogger.Abstraction;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using System.Net;
+
+namespace DatingSiteBackend.Extensions
+{
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static void ConfigureExceptionHandler(this IApplicationBuilder app, ILoggerManager logger)
+        {
+            app.UseExceptionHandler(appError => 
+            {
+                appError.Run(async context => 
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+
+                    if (contextFeature != null)
+                    {
+                        logger.LogError($"Error occured: {contextFeature.Error}");
+
+                        context.Response.AddApplicationError(contextFeature.Error.Message);
+                        await context.Response.WriteAsync(contextFeature.Error.Message);
+                    }
+                });    
+            });
+        }
+    }
+}
--- DatingSiteBackend/Startup.cs
+++ DatingSiteBackend/Startup.cs
@@ -1,16 +1,12 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using DatingSiteBackend.Extensions;
+using FileLogger.Abstraction;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using NLog;
 
 namespace DatingSiteBackend
@@ -40,11 +36,15 @@ namespace DatingSiteBackend
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerManager logger)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.ConfigureExceptionHandler(logger);
             }
 
             // Enables static files for request. (else it uses wwwroot by default)
